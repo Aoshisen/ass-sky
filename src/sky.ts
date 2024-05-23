88a3ab1a003eb445131b6d44463bdb75c6d7e11d
aoshisen <aoshisen@fanxiapp.com>
2024-05-23 15:59:53 +0800

    feat: 通过perlin noise 算法平滑过渡动画

--- src/sky.ts
+++ src/sky.ts
@@ -1,129 +1,61 @@
-interface Point {
-	x: PointValue,
-	y: PointValue,
-	opacity: Value
-}
-interface PointValue extends Value {
-	static_value: number
-}
+import { perlin } from "./utils/perlin-noise";
 
-interface Value {
-	value: number;
-	action: Action
-}
-enum Action {
-	ADD,
-	SUB
-}
+interface Point { x: number, y: number }
 export class Sky {
 	ctx: CanvasRenderingContext2D;
-	dots: Point[]
-	frames: number = 0
-	constructor(el: HTMLCanvasElement) {
-		this.ctx = el.getContext("2d")!
-		this.dots = this.getDotsPosition()
-		this.init()
-		this.startAnimation()
+	height: number = 0;
+	dots: any[] = [];
+	time: number = 0;
+
+	constructor(private el: HTMLCanvasElement) {
+		this.ctx = this.el.getContext("2d")!;
+		this.startAnimate();
 	}
-	init() {
-		this.dots.forEach((point) => {
-			this.dot(point)
-		})
-	}
-	getDotsPosition(): Point[] {
-		let result = []
-		for (let x = 0; x < this.ctx.canvas.width; x += 30) {
-			for (let y = 0; y < this.ctx.canvas.height; y += 30) {
-				result.push({
-					x: {
-						value: x,
-						static_value: x,
-						action: Math.random() > 0.5 ? Action.ADD : Action.SUB
-					}, y: {
-						value: y,
-						static_value: y,
-						action: Math.random() > 0.5 ? Action.ADD : Action.SUB
-					}, opacity: {
-						value: Math.random() * 0.5,
-						action: Math.random() > 0.5 ? Action.ADD : Action.SUB
-					}
-				})
+
+	getDots() {
+		const dots = [];
+		for (var x = 4; x < this.el.width; x += 20) {
+			for (var y = 4; y < this.el.height; y += 20) {
+				dots.push({ x, y });
 			}
 		}
-		return result;
+		return dots;
 	}
-	dot(p: Point) {
+	randomOpacity(x, y) {
+		let value = perlin(x / 50, y / 50, this.time * 0.01);
+		return value = (1 + value) * Math.pow(2, 6);
+	}
+	randomXPosition(x, y) {
+		let value2 = perlin(x / 50, y / 50, this.time * 0.01 + 10);
+		return value2 = (1 + value2) * 2 * Math.pow(2, 1);
+	}
+	randomYPosition(x, y) {
+		let value2 = perlin(x / 50, y / 50, this.time * 0.01 + 20);
+		return value2 = (1 + value2) * 2 * Math.pow(2, 1);
+	}
+
+	drawDot({ x, y }: Point) {
+		const opacity = this.randomOpacity(x, y)
+		const X = this.randomXPosition(x, y)
+		const Y = this.randomYPosition(x, y)
+		this.ctx.fillStyle = `rgba(${opacity},${opacity},${opacity},255)`;
 		this.ctx.beginPath();
-		this.ctx.arc(p.x.value, p.y.value, 1, 0, Math.PI * 2);
-		this.ctx.fillStyle = `rgba(255,255,255,${p.opacity.value})`;
+		this.ctx.arc(x + X, y + Y, 2, 0, Math.PI * 2);
 		this.ctx.fill();
-		this.ctx.closePath();
 	}
-	moveDots() {
-		this.dots = this.dots.map(dot => this.getNextDot(dot))
-	}
-	getNextDot(point: Point): Point {
-		return {
-			x: this.getNext(point.x),
-			y: this.getNext(point.y),
-			opacity: this.getNextOpacity(point.opacity)
-		}
-	}
-	getNext(p: PointValue) {
-		if (p.action === Action.ADD) {
-			const Max = p.static_value + 5
-			const nextValue = Math.min(Max, p.value + 0.01);
-			return {
-				...p,
-				value: nextValue,
-				action: nextValue >= Max ? Action.SUB : Action.ADD
-			}
-		}
-		else {
-			const Min = p.static_value - 5
-			const nextValue = Math.max(Min, p.value - 0.01);
-			return {
-				...p,
-				value: nextValue,
-				action: nextValue <= Min ? Action.ADD : Action.SUB
-			}
-		}
-	}
-	getNextOpacity(op: Value): Value {
-		if (op.action === Action.ADD) {
-			const value = Math.min(1, op.value + 0.002);
-			return {
-				value,
-				action: value >= 0.5 ? Action.SUB : Action.ADD
-			}
-		} else {
-			const value = Math.max(0, op.value - 0.002);
-			return {
-				value,
-				action: value <= 0 ? Action.ADD : Action.SUB
-			}
-		}
-	}
-	startAnimation() {
-		this.frames++
-		if (this.frames === 600) {
-			this.frames = 0
-		}
-		this.moveDots();
-		this.updateDots()
-		this.dots.forEach((point) => {
-			this.dot(point);
-		});
-		requestAnimationFrame(() => {
-			this.startAnimation();
+
+	dot() {
+		this.ctx.clearRect(0, 0, this.el.width, this.el.height);
+		this.dots = this.getDots();
+		this.dots.forEach(dot => {
+			this.drawDot(dot);
 		});
 	}
-	updateDots() {
-		this.dots.forEach((point) => {
-			// Clear previous dot
-			this.ctx.clearRect(point.x.value - 2, point.y.value - 2, 5, 5);
-			// Draw new dot
-			this.dot(point);
-		});
+
+	startAnimate() {
+		this.dot();
+		requestAnimationFrame(() => this.startAnimate());
+		this.time++
 	}
 }
+
